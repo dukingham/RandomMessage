1259b941a780770c12a670921b6242dab7bf5d39
Kenneth Falls IV <kennethfallsiv@MyPro.local>
2023-06-09 14:15:52 -0400

    changes made

--- randomMessage.js
+++ randomMessage.js
@@ -2,7 +2,7 @@ let nameFirst = 'Kenneth'
 let nameLast = 'Falls'
 
 const randMess = () => {
-    const messages = ['Hello World, My name is Kenneth Falls.', ' Hello, shall we play a game? ', 'I am alive! Mwahahahahaha! ', " Hey, how’s it going? "];
+    const messages = ['Hello World, My name is Kenneth Falls.', ' Hello, shall we play a game? ', 'I am alive! Mwahahahahaha! ', " Hey, how's it going? "];
     return messages [Math.floor(Math.random() * 4)];
 }
 

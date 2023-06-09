1b5a1f3adc57ea65a832698235f76f12c0c7b5d5
Kenneth Falls IV <kennethfallsiv@MyPro.local>
2023-06-09 14:12:03 -0400

    first commit

---
+++ randomMessage.js
@@ -0,0 +1,9 @@
+let nameFirst = 'Kenneth'
+let nameLast = 'Falls'
+
+const randMess = () => {
+    const messages = ['Hello World, My name is Kenneth Falls.', ' Hello, shall we play a game? ', 'I am alive! Mwahahahahaha! ', " Hey, how’s it going? "];
+    return messages [Math.floor(Math.random() * 4)];
+}
+
+console.log(randMess())
